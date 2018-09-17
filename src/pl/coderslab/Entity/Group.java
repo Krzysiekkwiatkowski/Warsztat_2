bc55d08fc9312b0abc5632af317f51461056c4d6
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2018-09-17 16:30:42 +0200

    Done basics of Group class functions

---
+++ src/pl/coderslab/Entity/Group.java
@@ -0,0 +1,92 @@
+package pl.coderslab.Entity;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Group {
+
+    private static String SAVE_GROUP = "INSERT INTO user_group (name) VALUES (?)";
+    private static String EDIT_GROUP = "UPDATE user_group SET name = ? WHERE id = ?";
+    private static String DELETE_GROUP = "DELETE FROM user_group WHERE id = ?";
+    private static String LOAD_GROUP_BY_ID = "SELECT * FROM user_group WHERE id = ?";
+    private static String LOAD_ALL_GROUPS = "SELECT * FROM user_group";
+
+    private int id;
+    private String name;
+
+    public Group() {
+    }
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void saveToDB(Connection connection) throws SQLException {
+        if(this.id == 0){
+            String[] generatedColumns = {"ID"};
+            PreparedStatement preparedStatement = connection.prepareStatement(SAVE_GROUP, generatedColumns);
+            preparedStatement.setString(1, this.name);
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            while (resultSet.next()){
+                this.id = resultSet.getInt(1);
+            }
+        } else {
+            PreparedStatement preparedStatement = connection.prepareStatement(EDIT_GROUP);
+            preparedStatement.setString(1, this.name);
+            preparedStatement.setInt(2, this.id);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public void delete(Connection connection) throws SQLException {
+        if(this.id != 0){
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_GROUP);
+            preparedStatement.setInt(1, this.id);
+            preparedStatement.executeUpdate();
+            this.id = 0;
+        }
+    }
+
+    public Group loadById(Connection connection, int id) throws SQLException {
+        if(this.id != 0){
+            PreparedStatement preparedStatement = connection.prepareStatement(LOAD_GROUP_BY_ID);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                Group loadedGroup = new Group();
+                loadedGroup.id = resultSet.getInt("id");
+                loadedGroup.name = resultSet.getString("name");
+                return loadedGroup;
+            }
+        }
+        return null;
+    }
+
+    public Group[] loadAll(Connection connection) throws SQLException {
+        ArrayList<Group> groups = new ArrayList<>();
+        PreparedStatement preparedStatement = connection.prepareStatement(LOAD_ALL_GROUPS);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()){
+            Group loadedGroup = new Group();
+            loadedGroup.id = resultSet.getInt("id");
+            loadedGroup.name = resultSet.getString("name");
+            groups.add(loadedGroup);
+        }
+        Group[] groupTable = new Group[groups.size()];
+        groupTable = groups.toArray(groupTable);
+        return groupTable;
+    }
+}
