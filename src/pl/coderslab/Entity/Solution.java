9c8407582c5df99695a50b77da09fa033538ef1c
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2018-10-09 14:52:47 +0200

    Fixed some issues

--- src/pl/coderslab/Entity/Solution.java
+++ src/pl/coderslab/Entity/Solution.java
@@ -226,7 +226,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/school?useSSL=false&characterEncoding=utf8", "root", "coderslab")){
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/warsztaty2?useSSL=false&characterEncoding=utf8", "root", "coderslab")){
             String option = "";
             Scanner scanner = new Scanner(System.in);
             Scanner scanner1 = new Scanner(System.in);
